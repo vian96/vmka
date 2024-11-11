ff46e61f498bf9f75537e97e54eabf4625ee2435
user.name <vianmart@yandex.ru>
2024-11-12 01:18:36 +0300

    first working simple asm

---
+++ cmds.hpp
@@ -0,0 +1,30 @@
+/*
+_CMD_DEF_(name, opcode, size, exec, read)
+*/
+
+#define _INT_SIZE_ (sizeof(int32_t))
+#define _PUSH_VAR_ code.push_back(cmd.args[iarg++]);
+#define _PUT_INT_(value) {code.resize(code.size()+_INT_SIZE_); \
+    *((int32_t*)&(code.data()[code.size()-_INT_SIZE_])) = value;}
+#define _PUSH_INT_ _PUT_INT_(cmd.args[iarg++])
+#define _POP_VAR_ARG_ (func->code[++pc])
+#define _POP_VAR_VAL_ (vars[func->code[++pc]])
+#define _POP_INT_ARG_ (*(uint32_t*)&(func->code.data()[(pc+=4)-3]))
+
+#define _DO_JMP_ { \
+    int tmp = _POP_INT_ARG_; DEB("dojmp" << tmp) \
+    new_pc = old_pc + tmp; }
+#define _PUSH_LABEL_OFFSET_ { \
+    int offset = _GET_LABEL_(cmd.args[iarg++]) - pc; DEB("dooffset" << offset)\
+    _PUT_INT_(offset) \
+}
+
+_CMD_DEF_(NOP, 0, 1, {}, {})
+_CMD_DEF_(INC, 1, 2, {vars[_POP_VAR_ARG_]++;}, {_PUSH_VAR_})
+_CMD_DEF_(STORE, 2, 6, {auto varind = _POP_VAR_ARG_; vars[varind] = _POP_INT_ARG_;}, {_PUSH_VAR_ _PUSH_INT_})
+_CMD_DEF_(ADD, 3, 3, {}, {_PUSH_VAR_ _PUSH_VAR_})
+_CMD_DEF_(EXIT, 4, 1, {return;}, {})
+_CMD_DEF_(PRINT, 5, 2, {std::cout << "out: " << vars[_POP_VAR_ARG_] << '\n';}, {_PUSH_VAR_})
+_CMD_DEF_(JMP, 6, 5, _DO_JMP_, _PUSH_LABEL_OFFSET_)
+_CMD_DEF_(JE, 7, 7, {int lhs=_POP_VAR_VAL_; int rhs=_POP_VAR_VAL_; if (lhs==rhs) _DO_JMP_}, {_PUSH_VAR_ _PUSH_VAR_ _PUSH_LABEL_OFFSET_})
+
---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+// #define DEBUG
+#ifdef DEBUG 
+#define DEB(out) std::cout << out << '\n';
+#else
+#define DEB(out) 
+#endif
+
+namespace InstrOpcodeNS { // so that i can write using
+enum InstrOpcode {
+LABEL = -1,
+#define _CMD_DEF_(name, opcode, argsnum, exec, read) name = opcode, 
+#include "cmds.hpp"
+#undef _CMD_DEF_
+};
+}
+
+struct CmdEntry {
+    InstrOpcodeNS::InstrOpcode opc;
+    std::vector<int> args;
+};
+        std::unordered_map<int, size_t> labels_map;
+
+class Function {
+public:
+    std::vector<uint8_t> code;
+    size_t num_vars;
+    size_t num_params;
+    Function(size_t nvars, size_t nparams, std::vector<CmdEntry> cmds)
+        : num_vars(nvars), num_params(nparams) {
+        for (const auto &cmd:cmds) {
+            int pc = code.size();
+            switch (cmd.opc) {
+                case InstrOpcodeNS::LABEL:
+                    labels_map[cmd.args[0]] = code.size();
+                    DEB((char) cmd.args[0] << code.size())
+                    continue;
+                #define _GET_LABEL_(ind) (0)
+                #define _CMD_DEF_(name, opcode, argsnum, exec, read) {\
+                case InstrOpcodeNS::name: \
+                    code.push_back(opcode); \
+                    size_t iarg = 0; \
+                    read \
+                    continue; }
+                #include "cmds.hpp"
+                #undef _CMD_DEF_
+                #undef _GET_LABEL_
+            }
+        }
+        code.resize(0);
+        for (const auto &cmd:cmds) {
+            int pc = code.size();
+            switch (cmd.opc) {
+                case InstrOpcodeNS::LABEL:
+                    continue;
+                #define _GET_LABEL_(ind) (labels_map[ind])
+                #define _CMD_DEF_(name, opcode, argsnum, exec, read) {\
+                case InstrOpcodeNS::name: \
+                    code.push_back(opcode); \
+                    size_t iarg = 0; \
+                    read \
+                    continue; }
+                #include "cmds.hpp"
+                #undef _CMD_DEF_
+                #undef _GET_LABEL_
+            }
+        }
+    }
+};
+
+class Frame {
+public:
+    Function *func;
+    std::vector<int32_t> vars;
+    int pc;
+    int new_pc;
+    void run() {
+        new_pc = 0;
+        while (true) {
+            pc = new_pc;
+            switch (func->code[pc]) {
+                #define _CMD_DEF_(name, opcode, size, exec, read) {\
+                case InstrOpcodeNS::name: \
+                    auto old_pc = pc; \
+                    new_pc = pc + size; \
+                    DEB(#name << pc << ' ' << new_pc); \
+                    exec \
+                    DEB(#name << pc << ' ' << new_pc); \
+                    continue; }
+                #include "cmds.hpp"
+            }
+        }
+    }
+    Frame(Function *func_) : func(func_), vars(std::vector<int32_t>(func->num_vars)), pc(0) {};
+};
+
+std::vector<Function*> functions;
+std::unordered_map<std::string, int> funcmap;
+
+int main() {
+    int status=0;
+    using namespace InstrOpcodeNS;
+    std::cout << "parse\n";
+    Function main_func(3, 0, {
+        {STORE, {0, 10}},
+        {STORE, {1, 17}},
+        {LABEL, {'s'}},
+        {INC, {0}},
+        {PRINT, {0}},
+        {JE, {0, 1, 'e'}},
+        {JMP, {'s'}},
+        {LABEL, {'e'}},
+        {EXIT, {}}
+    });
+    Frame fr(&main_func);
+    std::cout << "run\n";
+    fr.run();
+    std::cout << "exit\n";
+    return status;
+}
