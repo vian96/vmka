29a4757cf97a2352b7d88f3cb4bfec4131a49f92
user.name <vianmart@yandex.ru>
2024-11-28 18:43:34 +0300

    almost all fixes done

--- cmds.hpp
+++ cmds.hpp
@@ -2,28 +2,30 @@
 _CMD_DEF_(name, opcode, size, exec, read)
 */
 
-#define _INT_SIZE_ (sizeof(int32_t))
-#define _PUSH_VAR_ code.push_back(cmd.args[iarg++]);
-#define _PUT_INT_(value) {code.resize(code.size()+_INT_SIZE_); \
-    *((int32_t*)&(code.data()[code.size()-_INT_SIZE_])) = value;}
+// builder helpers
+#define _REG_SIZE_ (sizeof(RegType))
+#define _PUSH_VAR_ do { code.push_back(cmd.args[iarg++]); } while(0)
+#define _PUT_INT_(value) do {code.resize(code.size()+_REG_SIZE_); \
+    *((int32_t*)&(code.data()[code.size()-_REG_SIZE_])) = value;} while(0)
 #define _PUSH_INT_ _PUT_INT_(cmd.args[iarg++])
-#define _POP_VAR_ARG_ (func.code[++pc])
-#define _POP_VAR_VAL_ (vars[func.code[++pc]])
-#define _POP_INT_ARG_ (*(uint32_t*)&(func.code.data()[(pc+=4)-3]))
-
-#define _DO_JMP_ { \
-    int tmp = _POP_INT_ARG_; DEB("dojmp" << tmp) \
-    new_pc = old_pc + tmp; }
-#define _PUSH_LABEL_OFFSET_ { \
+#define _PUSH_LABEL_OFFSET_ do { \
     int offset = _GET_LABEL_(cmd.args[iarg++]) - pc; DEB("dooffset" << offset)\
-    _PUT_INT_(offset) \
-}
+    _PUT_INT_(offset); \
+} while(0)
 
-_CMD_DEF_(NOP, 0, 1, {}, {})
-_CMD_DEF_(INC, 1, 2, {vars[_POP_VAR_ARG_]++;}, {_PUSH_VAR_})
-_CMD_DEF_(STORE, 2, 6, {auto varind = _POP_VAR_ARG_; vars[varind] = _POP_INT_ARG_;}, {_PUSH_VAR_ _PUSH_INT_})
+// executor helpers
+#define _POP_VAR_ARG_ (func.code[++frame->pc])
+#define _POP_VAR_VAL_ (frame->vars[func.code[++frame->pc]])
+#define _POP_INT_ARG_ (*(uint32_t*)&(func.code[(frame->pc+=4)-3]))
+#define _DO_JMP_ do { \
+    int tmp = _POP_INT_ARG_; DEB("dojmp" << tmp) \
+    frame->new_pc = old_pc + tmp; } while(0)
 
-#define _BIN_OP(name, ind, op) _CMD_DEF_(name, ind, 4, {int vard = _POP_VAR_ARG_; int lhs=_POP_VAR_VAL_; int rhs=_POP_VAR_VAL_; vars[vard] = lhs op rhs;}, {_PUSH_VAR_ _PUSH_VAR_ _PUSH_VAR_})
+_CMD_DEF_(NOP, 0, 1, do {} while(0), do {} while(0))
+_CMD_DEF_(INC, 1, 2, do {frame->vars[_POP_VAR_ARG_]++;} while(0), do {_PUSH_VAR_;} while(0))
+_CMD_DEF_(STORE, 2, 6, do {auto varind = _POP_VAR_ARG_; frame->vars[varind] = _POP_INT_ARG_;} while(0), do {_PUSH_VAR_; _PUSH_INT_;} while(0))
+
+#define _BIN_OP(name, ind, op) _CMD_DEF_(name, ind, 4, do {int vard = _POP_VAR_ARG_; int lhs=_POP_VAR_VAL_; int rhs=_POP_VAR_VAL_; frame->vars[vard] = lhs op rhs;} while(0), do {_PUSH_VAR_; _PUSH_VAR_; _PUSH_VAR_;} while(0))
 
 _BIN_OP(ADD, 20, +)
 _BIN_OP(SUB, 21, -)
@@ -33,12 +35,12 @@ _BIN_OP(MOD, 24, %)
 
 #undef _BIN_OP
 
-_CMD_DEF_(EXIT, 4, 1, {return 0;}, {})
-_CMD_DEF_(PRINT, 5, 2, {std::cout << "out: " << vars[_POP_VAR_ARG_] << '\n';}, {_PUSH_VAR_})
+_CMD_DEF_(EXIT, 4, 1, do {return 0;} while(0), do {} while(0))
+_CMD_DEF_(PRINT, 5, 2, do {std::cout << "out: " << frame->vars[_POP_VAR_ARG_] << '\n';} while(0), do {_PUSH_VAR_;} while(0))
 
-_CMD_DEF_(JMP, 6, 5, _DO_JMP_, _PUSH_LABEL_OFFSET_)
+_CMD_DEF_(JMP, 6, 5, _DO_JMP_;, _PUSH_LABEL_OFFSET_;)
 
-#define _BIN_COND_JMP_(name, ind, op) _CMD_DEF_(name, ind, 7, {int lhs=_POP_VAR_VAL_; int rhs=_POP_VAR_VAL_; if (lhs op rhs) _DO_JMP_}, {_PUSH_VAR_ _PUSH_VAR_ _PUSH_LABEL_OFFSET_})
+#define _BIN_COND_JMP_(name, ind, op) _CMD_DEF_(name, ind, 7, do {int lhs=_POP_VAR_VAL_; int rhs=_POP_VAR_VAL_; if (lhs op rhs) _DO_JMP_;} while(0), do {_PUSH_VAR_; _PUSH_VAR_; _PUSH_LABEL_OFFSET_;} while(0))
 
 _BIN_COND_JMP_(JEQ, 30, ==)
 _BIN_COND_JMP_(JNE, 31, !=)
@@ -47,14 +49,10 @@ _BIN_COND_JMP_(JLE, 33, <=)
 _BIN_COND_JMP_(JGE, 34, >=)
 _BIN_COND_JMP_(JGT, 35, >)
 
-_CMD_DEF_(INPUT, 9, 2, {auto varind = _POP_VAR_ARG_; std::cout << "input: "; std::cin >> vars[varind];}, {_PUSH_VAR_})
-_CMD_DEF_(MOV, 10, 3, {int vard = _POP_VAR_ARG_; int src=_POP_VAR_VAL_; vars[vard] = src;}, {_PUSH_VAR_ _PUSH_VAR_})
+_CMD_DEF_(INPUT, 9, 2, do {auto varind = _POP_VAR_ARG_; std::cout << "input: "; std::cin >> frame->vars[varind];} while(0), do {_PUSH_VAR_;} while(0))
+_CMD_DEF_(MOV, 10, 3, do {int vard = _POP_VAR_ARG_; int src=_POP_VAR_VAL_; frame->vars[vard] = src;} while(0), do {_PUSH_VAR_; _PUSH_VAR_;} while(0))
 
-#define _GET_ARG_I_(i) int arg##i = _POP_VAR_VAL_;
-#define _CALL_N_(n, ind, args_read, args_push, ...) _CMD_DEF_(CALL##n, ind, 6+n, {int vard = _POP_VAR_ARG_; int funcind = _POP_INT_ARG_; args_read; Frame fr(interp.functions[funcind], {__VA_ARGS__}); vars[vard] = fr.run(interp);}, {_PUSH_VAR_ _PUSH_INT_ args_push})
+_CMD_DEF_(CALL0, 40, 6, do {int vard = _POP_VAR_ARG_; int funcind = _POP_INT_ARG_; frame->vars[vard] = run_func(funcind, {}); printf("res=%d\n", frame->vars[vard]); frame = frame->prev;} while(0), do {_PUSH_VAR_; _PUSH_INT_;} while(0))
+_CMD_DEF_(CALL1, 41, 7, do {int vard = _POP_VAR_ARG_; int funcind = _POP_INT_ARG_; RegType res = run_func(funcind, {_POP_VAR_VAL_}); frame = frame->prev;frame->vars[vard] = res;} while(0), do {_PUSH_VAR_; _PUSH_INT_; _PUSH_VAR_;} while(0))
 
-_CALL_N_(1, 41, _GET_ARG_I_(0), _PUSH_VAR_, arg0)
-_CALL_N_(2, 42, _GET_ARG_I_(0) _GET_ARG_I_(1), _PUSH_VAR_ _PUSH_VAR_, arg0, arg1)
-_CALL_N_(3, 43, _GET_ARG_I_(0) _GET_ARG_I_(1) _GET_ARG_I_(2), _PUSH_VAR_ _PUSH_VAR_ _PUSH_VAR_, arg0, arg1, arg2)
-
-_CMD_DEF_(RET, 13, 2, { return vars[_POP_VAR_ARG_];}, {_PUSH_VAR_})
+_CMD_DEF_(RET, 13, 2, do { return frame->vars[_POP_VAR_ARG_];} while(0), do {_PUSH_VAR_;} while(0))
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <algorithm>
+#include <alloca.h>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
 #include <cstdlib>
-#include <string>
+#include <sys/types.h>
 #include <unordered_map>
 #include <vector>
 #include <iostream>
@@ -23,107 +24,115 @@ LABEL = -1,
 };
 }
 
+typedef int32_t RegType;
+
 struct CmdEntry {
     InstrOpcodeNS::InstrOpcode opc;
     std::vector<int> args;
 };
 
-class Function {
-public:
-    std::vector<uint8_t> code;
-    size_t num_vars;
-    size_t num_params;
-    Function(size_t nvars, size_t nparams, std::vector<CmdEntry> cmds)
-        : num_vars(nvars), num_params(nparams) {
-        // TODO remove doubled code
-        std::unordered_map<int, size_t> labels_map;
-        for (const auto &cmd:cmds) {
-            int pc = code.size();
-            switch (cmd.opc) {
-                case InstrOpcodeNS::LABEL:
-                    labels_map[cmd.args[0]] = code.size();
-                    DEB((char) cmd.args[0] << code.size())
-                    continue;
-                #define _GET_LABEL_(ind) (0)
-                #define _CMD_DEF_(name, opcode, argsnum, exec, read) {\
-                case InstrOpcodeNS::name: \
-                    code.push_back(opcode); \
-                    size_t iarg = 0; \
-                    read \
-                    continue; }
-                #include "cmds.hpp"
-                #undef _CMD_DEF_
-                #undef _GET_LABEL_
-            }
-        }
-        code.resize(0);
-        for (const auto &cmd:cmds) {
-            int pc = code.size();
-            switch (cmd.opc) {
-                case InstrOpcodeNS::LABEL:
-                    continue;
-                #define _GET_LABEL_(ind) (labels_map[ind])
-                #define _CMD_DEF_(name, opcode, argsnum, exec, read) {\
-                case InstrOpcodeNS::name: \
-                    code.push_back(opcode); \
-                    size_t iarg = 0; \
-                    read \
-                    continue; }
-                #include "cmds.hpp"
-                #undef _CMD_DEF_
-                #undef _GET_LABEL_
-            }
-        }
-    }
+struct CallArgs {
+    RegType arg0, arg1;
 };
 
-class Interpreter;
+std::vector<uint8_t> build_bytecode(std::vector<CmdEntry> cmds) {
+    std::vector<uint8_t> code;
+    std::unordered_map<int, size_t> labels_map;
+    #define _CMD_DEF_(name, opcode, argsnum, exec, read) {\
+    case InstrOpcodeNS::name: \
+        code.push_back(opcode); \
+        size_t iarg = 0; \
+        read; \
+        continue; }
+
+    #define _GET_LABEL_(ind) (0)
+    for (const auto &cmd:cmds) {
+        int pc = code.size();
+        switch (cmd.opc) {
+            case InstrOpcodeNS::LABEL:
+                labels_map[cmd.args[0]] = code.size();
+                DEB((char) cmd.args[0] << code.size())
+                continue;
+            #include "cmds.hpp"
+            #undef _GET_LABEL_
+        }
+    }
+    code.resize(0);
+    #define _GET_LABEL_(ind) (labels_map[ind])
+    for (const auto &cmd:cmds) {
+        int pc = code.size();
+        switch (cmd.opc) {
+            case InstrOpcodeNS::LABEL:
+                continue;
+            #include "cmds.hpp"
+            #undef _CMD_DEF_
+            #undef _GET_LABEL_
+        }
+    }
+    DEB("code size: " << code.size());
+    return code;
+}
+
+class Function {
+public:
+    uint8_t *code;
+    size_t num_vars;
+    size_t num_params;
+    Function(size_t nvars, size_t nparams, uint8_t *code_)
+        : num_vars(nvars), num_params(nparams), code(code_) {}
+};
 
 class Frame {
 public:
     const Function &func;
-    std::vector<int32_t> vars;
     int pc;
     int new_pc;
-    Frame(const Function &func_) : func(func_), vars(std::vector<int32_t>(func.num_vars)), pc(0) {}
-    Frame(const Function &func_, std::vector<int32_t> params) : func(func_), vars(std::vector<int32_t>(func.num_vars)), pc(0) {
-        std::copy(params.begin(), params.end(), vars.begin());
-    }
-    int run(Interpreter &executor);
+    Frame *prev;
+    RegType vars[];
+
+    Frame(Frame *prev_, const Function &func_) : prev(prev_), func(func_), pc(0) {}
 };
 
-class Interpreter {
+class Runtime {
 public:
     std::vector<Function> functions;
-    int run_func(size_t func_ind, std::vector<int32_t> params={}) {
-        Frame fr(functions[func_ind], params);
-        return fr.run(*this);
+    Frame *frame;
+    RegType run_func(size_t func_ind, CallArgs args) {
+        auto &func = functions[func_ind];
+        frame = new(alloca(sizeof(Frame) + func.num_vars*sizeof(RegType))) Frame(frame, func);
+        if (frame->func.num_params)
+            frame->vars[0] = args.arg0;
+        if (frame->func.num_params > 1)
+            frame->vars[1] = args.arg1;
+        //printf("arg0=%d arg1=%d num=%d\n", args.arg0, args.arg1, func.num_params);
+        //printf("arg0=%d arg1=%d\n", frame->vars[0], frame->vars[1]);
+
+        frame->new_pc = 0;
+        while (true) {
+            frame->pc = frame->new_pc;
+            switch (func.code[frame->pc]) {
+                #define _CMD_DEF_(name, opcode, size, exec, read) {\
+                case InstrOpcodeNS::name: \
+                    auto old_pc = frame->pc; \
+                    frame->new_pc = frame->pc + size; \
+                    DEB(#name << frame->pc << ' ' << frame->new_pc); \
+                    exec; \
+                    DEB(#name << frame->pc << ' ' << frame->new_pc); \
+                    continue; }
+                #include "cmds.hpp"
+            }
+        }
     }
 };
 
-int Frame::run(Interpreter &interp) {
-    new_pc = 0;
-    while (true) {
-        pc = new_pc;
-        switch (func.code[pc]) {
-            #define _CMD_DEF_(name, opcode, size, exec, read) {\
-            case InstrOpcodeNS::name: \
-                auto old_pc = pc; \
-                new_pc = pc + size; \
-                DEB(#name << pc << ' ' << new_pc); \
-                exec \
-                DEB(#name << pc << ' ' << new_pc); \
-                continue; }
-            #include "cmds.hpp"
-        }
-    }
-}
+Runtime runtime;
 
 int main() {
     int status=0;
     using namespace InstrOpcodeNS;
     std::cout << "parse\n";
-    Function main_func(3, 0, {
+    
+    auto main_code = build_bytecode({
         {STORE, {0, 10}},
 //        {STORE, {1, 17}},
         {INPUT, {1}},
@@ -135,14 +144,18 @@ int main() {
         {LABEL, {'e'}},
         {EXIT, {}}
     });
-    Function sum_func(3, 0, {
+    Function main_func(3, 0, main_code.data());
+
+    auto sum_code = build_bytecode({
         {STORE, {0, 10}},
         {INPUT, {1}},
         {ADD, {1, 1, 0}},
         {PRINT, {1}},
         {RET, {1}}
     });
-    Function fib_iter_func(3, 0, {
+    Function sum_func(3, 0, sum_code.data());
+
+    auto fib_iter_code = build_bytecode({
         // v0 i v1 n v2 f0 v3 f1 v4 t
         {INPUT, {1}},
         {STORE, {0, 2}},
@@ -159,13 +172,15 @@ int main() {
         {PRINT, {3}},
         {RET, {3}}
     });
-
-    Function fib_rec_func(5, 1, {
+    Function fib_iter_func(3, 0, fib_iter_code.data());
+ 
+    auto fib_rec_code = build_bytecode({
         // v0 n v1 n-1(-2) v2 fib(n-1) v3 fib(n-2) v4 -1holder
         {STORE, {4, 1}},
         {JLE, {0, 4, 's'}},
         {STORE, {4, -1}},
         {ADD, {0, 0, 4}},
+      //  {PRINT, {0}},
         {CALL1, {2, 2, 0}}, // fib(n-1)
        //  {PRINT, {0}},
        // {PRINT, {2}},
@@ -178,15 +193,15 @@ int main() {
         {LABEL, {'s'}},
         {RET, {0}}
     });
-
-    Interpreter interp;
-    interp.functions = {main_func, fib_iter_func, fib_rec_func};
+    Function fib_rec_func(5, 1, fib_rec_code.data());
+ 
+    runtime.functions = {main_func, fib_iter_func, fib_rec_func};
 
     std::cout << "count from 10 to input:\n";
-    interp.run_func(0);
+    runtime.run_func(0, {});
     std::cout << "iter fib(input): ";
-    interp.run_func(1);
-    std::cout << "recursive fib(11): " << interp.run_func(2, {11}) << '\n';
-    std::cout << "finishing..";
+    runtime.run_func(1, {});
+    std::cout << "recursive fib(11): " << runtime.run_func(2, {11}) << '\n';
+    std::cout << "finishing..\n";
     return status;
 }
